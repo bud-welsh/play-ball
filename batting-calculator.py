8377bdf04732749d4c0ddbb2e27cd2c2a8e71722
Bud Welsh <budwelsh14@gmail.com>
2020-01-06 15:26:12 -0600

    Add Hall of Fame information

--- batting-calculator.py
+++ batting-calculator.py
@@ -1,6 +1,12 @@
 player = "Babe Ruth"
+hall_of_fame = True
 hits = 2873
 at_bats = 10623
 
 print(player + " Batting Average")
 print(hits / at_bats)
+
+if hall_of_fame == True:
+    print(player + " is in the Hall of Fame")
+else:
+    print(player + " is not in the Hall of Fame")
