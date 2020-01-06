28f1c3ddcfbb27cee85fcfc40fe49ce030eb6047
Bud Welsh <budwelsh14@gmail.com>
2020-01-06 16:12:30 -0600

    Add total bases input and formula

--- batting-calculator.py
+++ batting-calculator.py
@@ -2,8 +2,13 @@ print('Welcome to the Batting Calculator')
 
 player_name = input('What is the name or the player? ')
 hits = int(input('How many hits did ' + player_name + ' have during his career? '))
+doubles = int(input('How many of those hits were doubles? '))
+triples = int(input('How many of those hits were triples? '))
+home_runs = int(input('How many of those hits were home runs? '))
 at_bats = int(input('How many at bats did ' + player_name + ' have during his career? '))
 batting_average = hits / at_bats
+total_bases = (hits + doubles + (triples * 2) + (home_runs * 3))
 
-print(player_name + ' had a batting average of:')
-print(batting_average)
+print(player_name + ' had a batting average of: ' + str(batting_average))
+print(player_name + ' had ' + str(total_bases) + ' total bases')
+
